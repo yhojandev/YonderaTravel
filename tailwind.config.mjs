f627b1ec7c9e45f30e4958a8eb3af771005ce410
yhojan <yhojan5416@gmail.com>
2024-12-24 09:42:42 -0500

    cambios header

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,26 +12,6 @@ export default {
         txtDarkPrimary: '#E0E0E0', // Texto principal en modo oscuro
         txtDarkSecondary: '#B3B3B3', // Texto secundario en modo oscuro
       },
-
-      fontSize: {
-        // Tamaños para dispositivos grandes (escritorio)
-        'title-lg': ['40px', '1.2'], // Título principal
-        'subtitle-lg': ['24px', '1.4'], // Subtítulos
-        'body-lg': ['18px', '1.6'], // Texto normal
-        'note-lg': ['14px', '1.6'], // Notas o texto secundario
-
-        // Tamaños para dispositivos pequeños (móviles)
-        'title-sm': ['28px', '1.2'], // Título principal
-        'subtitle-sm': ['20px', '1.4'], // Subtítulos
-        'body-sm': ['16px', '1.6'], // Texto normal
-        'note-sm': ['14px', '1.6'], // Notas o texto secundario
-      },
-
-      backgroundImage: {
-        backgroundWhite: 'bg-white bg-opacity-40 backdrop-saturate-[180%] backdrop-blur-[20px]',
-        backgroundDark:
-          'dark:bg-gray-900 dark:bg-opacity-40 dark:backdrop-saturate-[180%] dark:backdrop-blur-[20px]',
-      },
     },
   },
   plugins: [],
